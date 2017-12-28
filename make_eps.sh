92742f102c78f8b573c4efceb38756b2d826651a
Luiz Gavaza <gavaza@gmail.com>
2017-12-28 19:08:38 -0300

    [bugfix] Correção nos dados referentes aos problemas do semestre 2017. Adicionado para criação de gráficos referente a disciplina. Adicionado dados referentes a percepção da disciplina para os concluintes de 2017. Adicionado dados referentes a percepção da disciplina para os desistentes de 2016. Mais informações sobre a coleta de dados na capítulo de resultados.

--- make_eps.sh
+++ make_eps.sh
@@ -5,11 +5,14 @@ ls -1 notas-*.dat | while read -r i; do
 	./contar.out < $i > g$i
 	gnuplot -e "fileout='$(basename "$i" .dat).eps'; filein='g$i'" histograms.4.gnu
 done
+
 ls -1 gnotas-*.dat | while read -r i; do
 	rm -rf "$i"
 done
 rm -rf contar.out
 
-ls -1 question-*.csv | while read -r i; do
+ls -1 question-*.csv concluintes-*.csv desistentes-*.csv | while read -r i; do
 	gnuplot -e "fileout='$(basename "$i" .csv).eps'; filein='$i'" histograms.5.gnu
 done
+
+exit 0
